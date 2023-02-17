d70df7428e1463acb3208ca3630e9d26b2b86ab8
Khusir-Redapple <77429680+Khusir-Redapple@users.noreply.github.com>
2023-02-17 11:32:20 +0530

    bug_fixed_37

--- socket/controller/_table.js
+++ socket/controller/_table.js
@@ -172,7 +172,41 @@ module.exports = {
                     },
                 };
                 resObj.events.push(event);
-            } else {
+            } 
+            /**
+             * New logic here, extra case
+             */
+            else if(movePossible == false && DICE_ROLLED ==6){
+                console.log('[DICE ROLLED NOT SIX]');
+                //  SCRAP CURRENT DICES & PASS NEXT DICE_ROLL
+                await _tab.scrapTurn(params.room, myPos);
+                // DICE_ROLL TO NEXT
+                let timer = 1500;
+                let nextPos = await _tab.getNextPosition(params.room, myPos);
+                await _tab.updateCurrentTurn(params.room, nextPos, 'roll', myPos);
+                let dices_rolled = await _tab.gePlayerDices(params.room, nextPos);
+                let DICE_ROLLED = await _tab.rollDice();
+                await _tab.diceRolled(params.room, nextPos, DICE_ROLLED);
+                await _tab.sedAndResetGamePlayData(params.room);
+                let event = {
+                    type: 'room_including_me',
+                    room: params.room,
+                    delay: timer,
+                    name: 'make_diceroll',
+                    data: {
+                        room: params.room,
+                        position: nextPos,
+                        tokens: _tab.getTokens(params.room),
+                        dice: DICE_ROLLED,
+                        dices_rolled: dices_rolled,
+                        turn_start_at: config.turnTimer,
+                        extra_move_animation:false
+                    },
+                };
+                resObj.events.push(event);
+            }
+            
+            else {
                 console.log('[DICE ROLLED SIX]');
                 // await _tab.addBonus(params.room, id, 1);
                 // Send 'roll' to same player
@@ -206,40 +240,40 @@ module.exports = {
          * Bug No: 37
          * when the movable pawn has less than 6 steps to move then we are getting extra move
          */
-        if (dices_rolled[0] == 6 && movePossible == false) {
-            // Bug No : 37::
-             //  SCRAP CURRENT DICES & PASS NEXT DICE_ROLL
-            _tab.scrapTurn(params.room, myPos);             
+        // if (dices_rolled[0] == 6 && movePossible == false) {
+        //     // Bug No : 37::
+        //      //  SCRAP CURRENT DICES & PASS NEXT DICE_ROLL
+        //     _tab.scrapTurn(params.room, myPos);             
 
-             // DICE_ROLL TO NEXT
-             let nextPos = await _tab.getNextPosition(params.room, myPos);
+        //      // DICE_ROLL TO NEXT
+        //      let nextPos = await _tab.getNextPosition(params.room, myPos);
 
-            // added to 
-           _tab.scrapTurn(params.room, nextPos);
-           _tab.updateCurrentTurn(params.room, nextPos, 'roll', myPos);
+        //     // added to 
+        //    _tab.scrapTurn(params.room, nextPos);
+        //    _tab.updateCurrentTurn(params.room, nextPos, 'roll', myPos);
 
-            // await _tab.updateCurrentTurn(params.room, nextPos, 'roll', myPos);
-             let dices_rolled = await _tab.gePlayerDices(params.room, nextPos);
-             let DICE_ROLLED = await _tab.rollDice();
-             await _tab.diceRolled(params.room, nextPos, DICE_ROLLED);
-             await _tab.sedAndResetGamePlayData(params.room);
-             let event = {
-                 type: 'room_including_me',
-                 room: params.room,
-                 delay: 2000,
-                 name: 'make_diceroll',
-                 data: {
-                     room: params.room,
-                     position: nextPos,
-                     tokens: _tab.getTokens(params.room),
-                     dice: DICE_ROLLED,
-                     dices_rolled: dices_rolled,
-                     turn_start_at: config.turnTimer,
-                     extra_move_animation:false
-                 },
-             };
-            resObj.events.push(event);
-        }
+        //     // await _tab.updateCurrentTurn(params.room, nextPos, 'roll', myPos);
+        //      let dices_rolled = await _tab.gePlayerDices(params.room, nextPos);
+        //      let DICE_ROLLED = await _tab.rollDice();
+        //      await _tab.diceRolled(params.room, nextPos, DICE_ROLLED);
+        //      await _tab.sedAndResetGamePlayData(params.room);
+        //      let event = {
+        //          type: 'room_including_me',
+        //          room: params.room,
+        //          delay: 2000,
+        //          name: 'make_diceroll',
+        //          data: {
+        //              room: params.room,
+        //              position: nextPos,
+        //              tokens: _tab.getTokens(params.room),
+        //              dice: DICE_ROLLED,
+        //              dices_rolled: dices_rolled,
+        //              turn_start_at: config.turnTimer,
+        //              extra_move_animation:false
+        //          },
+        //      };
+        //     resObj.events.push(event);
+        // }
 
         let events = {
             type: 'room_including_me',
