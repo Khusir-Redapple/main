69c9ae7189bfb2b661cdbba9f2931f91ce337938
Khusir-Redapple <77429680+Khusir-Redapple@users.noreply.github.com>
2023-03-13 15:24:33 +0530

    refactoring request-template

--- api/service/index.js
+++ api/service/index.js
@@ -1,10 +1,9 @@
-
-var ObjectId = require('mongoose').Types.ObjectId;
-var jwt = require('jsonwebtoken');
-var config = require('./../../config'); 
+const ObjectId  = require('mongoose').Types.ObjectId;
+const jwt       = require('jsonwebtoken');
 
 module.exports = {
-    response: function(status, message, data) {
+    response: function (status, message, data)
+    {
         return {
             status: status,
             message: message,
@@ -12,24 +11,30 @@ module.exports = {
         };
     },
 
-    validateObjectId: function(id) {
-        if (ObjectId.isValid(id)) {
+    validateObjectId: function (id)
+    {
+        if (ObjectId.isValid(id))
+        {
             var obj = new ObjectId(id);
-            if (obj == id) {
+            if (obj == id)
+            {
                 return true;
             }
         }
         return false;
     },
 
-    randomNumber: async function(length) {
+    randomNumber: async function (length)
+    {
         return Math.floor(
             Math.pow(10, length - 1) + Math.random() * (Math.pow(10, length) - Math.pow(10, length - 1) - 1)
         );
     },
-    issueToken: function (data) {
-        console.log(typeof(data))
-        if( typeof(data) != 'object') data = JSON.parse(data);
+    
+    issueToken: function (data)
+    {
+        console.log(typeof (data))
+        if (typeof (data) != 'object') data = JSON.parse(data);
         //Reading JWT secret key from process.env.API_SECRET_KEY in SSM
         return jwt.sign(data, process.env.API_SECRET_KEY, {expiresIn: 604800});
     },
--- api/service/request-template.js
+++ api/service/request-template.js
@@ -1,65 +1,69 @@
-const axios = require('axios').default;
-const queryString = require('query-string');
-const config = require('../../config')
-var Service = require('../../api/service');
-const logDNA   = require('../../api/service/logDNA');
-async function call(path, method, body, headers = {}) {
-    
-    try{
+const axios         = require('axios').default;
+const queryString   = require('query-string');
+const Service       = require('../../api/service');
+const logDNA        = require('../../api/service/logDNA');
+
+async function call(path, method, body, headers = {})
+{
+    try
+    {
         const url = process.env.VERIFY_USER_URL + path;
         let token = await Service.issueToken(body);
-        console.log("PAth ::",path,url,body, 'Bearer ' + token , new Date())
+        console.log("PAth ::", path, url, body, 'Bearer ' + token, new Date())
         // for logDNA 
         var logData = {
             level: 'debugg',
             meta: {
-                    "url" : url,
-                    "path" : path
-                }
-          };        
+                "url": url,
+                "path": path
+            }
+        };
         logDNA.log('verifyUser_url', logData);
 
         let apiResponce = await axios.request({
             url,
             method,
             data: body,
-            headers:{ 
+            headers: {
                 "authorization": 'Bearer ' + token
             },
         });
         let data = apiResponce.data;
-        console.log(path , " Data resp. is > ", data)
-        if(data.isSuccess == true){
+        console.log(path, " Data resp. is > ", data)
+        if (data.isSuccess == true)
+        {
             return data;
         }
-        else 
-            return { isSuccess: false, error: data.error };
+        else
+            return {isSuccess: false, error: data.error};
 
-    } catch (err) {
+    } catch (err)
+    {
         console.log(err);
         // for logDNA 
-        var logData = {
+        let logData = {
             level: 'debugg',
             meta: err
-          };        
+        };
         logDNA.log('verifyUserError', logData);
-        return { isSuccess: false } ;
+        return {isSuccess: false};
 
     }
 }
 
-function get(path, query, headers = {}) {
-    if (query) {
+function get(path, query, headers = {})
+{
+    if (query)
+    {
         path += `?${queryString.stringify(query)}`;
     }
-    return call(path,'GET', {}, headers);
+    return call(path, 'GET', {}, headers);
 }
 
-function post(path, body, headers = {}) {
-    return call(path,'POST', body, headers);
+function post(path, body, headers = {})
+{
+    return call(path, 'POST', body, headers);
 }
-
-
 
 module.exports = {
     call,
