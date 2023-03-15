5887e1c60378b7e03b9d2f188eb172fd99868612
Khusir-Redapple <77429680+Khusir-Redapple@users.noreply.github.com>
2023-03-15 11:52:55 +0530

    code refactorial

--- socket/controller/message_controllers.js
+++ socket/controller/message_controllers.js
@@ -1,54 +1,56 @@
 'use strict';
 
-const {
-    sqsSendMessage
-} = require('../../api/operations/sqs_operations');
-const { User }  = require('../../api/models/user');
-const logDNA   = require('../../api/service/logDNA');
-class MessageController {
+const {sqsSendMessage}  = require('../../api/operations/sqs_operations');
+const {User}            = require('../../api/models/user');
+const logDNA            = require('../../api/service/logDNA');
+class MessageController
+{
 
     /**
      * Send message from this route handler
      */
-    async sendMessage(req) {
-        try {
+    async sendMessage(req)
+    {
+        try
+        {
             let id = new Date().getTime();
             id = String(id);
-            const params = { 
-                MessageBody: JSON.stringify(req.data), 
+            const params = {
+                MessageBody: JSON.stringify(req.data),
                 // MessageGroupId: id,
                 // MessageDeduplicationId: id,
             };
-            console.log("sendMessage data:: >",params);
+            console.log("sendMessage data:: >", params);
             // JSON to object convert
             let parseData = JSON.parse(params.MessageBody);
             // db search for lobbyId
             let userData = await User.findOne({
-                numeric_id : parseData.User
+                numeric_id: parseData.User
             });
             // To add lobbyId in existing object   
             parseData.lobbyId = userData.lobbyId;
             // To build final object
-            let finalParams = { MessageBody : JSON.stringify(parseData)};
-            console.log("after adding lobbyId:: >",finalParams)      
+            let finalParams = {MessageBody: JSON.stringify(parseData)};
+            console.log("after adding lobbyId sendMessage data:: >", finalParams)
             let result = await sqsSendMessage(finalParams);
             // for SQS Success
-            var logData = {
+            let logData = {
                 level: 'debugg',
                 meta: result
-            };        
+            };
             logDNA.log('SQS sendMessage Success', logData);
 
             return result;
-        } catch(error) {
+        } catch (error)
+        {
             // for SQS Success
-            var logData = {
+            let logData = {
                 level: 'debugg',
                 meta: error
-            };        
+            };
             logDNA.log('SQS sendMessage ERROR', logData);
-            
-            console.log("SQS sendMessage ERROR :: ",error)
+
+            console.log("SQS sendMessage ERROR :: ", error)
             return false;
         }
     }
--- socket/helper/sockets.js
+++ socket/helper/sockets.js
@@ -1,27 +1,30 @@
-var logger = require('../../api/service/logger');
-const { User } = require('./../../api/models/user');
-const Table = require('./../../api/models/table');
+const {User}    = require('./../../api/models/user');
+const Table     = require('./../../api/models/table');
 
-class Sockets {
-    constructor() {
+class Sockets
+{
+    constructor()
+    {
         this.currentUsers = [];
     }
 
-    updateSocket(id, socket) {
-        var flag = false;
-        for (var i = 0; i < this.currentUsers.length; i++) {
-            if (this.currentUsers[i].data_id.equals(id)) {
+    updateSocket(id, socket)
+    {
+        let flag = false;
+        for (let i = 0; i < this.currentUsers.length; i++)
+        {
+            if (this.currentUsers[i].data_id.equals(id))
+            {
                 flag = true;
                 this.currentUsers[i].socket = socket.id;
                 this.currentUsers[i].socketIS = socket;
                 this.currentUsers[i].status = 'online';
                 this.currentUsers[i].last_seen = 0;
-            } else {
-                // logger.info(`${this.currentUsers[i].data_id} is not same as ${id}`);
             }
         }
 
-        if (!flag) {
+        if (!flag)
+        {
             this.currentUsers.push({
                 data_id: id,
                 socket: socket.id,
@@ -30,71 +33,81 @@ class Sockets {
                 last_seen: 0
             });
         }
-
-        // logger.info("SOCKETS",this.currentUsers);
         return true;
     }
 
-    getSocket(id) {
-        for (var i = 0; i < this.currentUsers.length; i++) {
-            console.log("getSocket >>>", typeof this.currentUsers[i].data_id,typeof id,this.currentUsers[i].data_id.toString() == id.toString())
-            if (this.currentUsers[i].data_id.toString() == id.toString()) {
+    getSocket(id)
+    {
+        for (let i = 0; i < this.currentUsers.length; i++)
+        {
+            console.log("getSocket >>>", typeof this.currentUsers[i].data_id, typeof id, this.currentUsers[i].data_id.toString() == id.toString())
+            if (this.currentUsers[i].data_id.toString() == id.toString())
+            {
                 return this.currentUsers[i].socket;
-            } else {
-                // logger.info(this.currentUsers[i].data_id + ' != ' + id);
             }
         }
         return false;
     }
 
-    getSocketIS(id) {
-        for (var i = 0; i < this.currentUsers.length; i++) {
-            console.log(this.currentUsers[i].data_id,id,this.currentUsers[i].socketIS)
-            if (this.currentUsers[i].data_id == id) {
+    getSocketIS(id)
+    {
+        for (let i = 0; i < this.currentUsers.length; i++)
+        {
+            console.log(this.currentUsers[i].data_id, id, this.currentUsers[i].socketIS)
+            if (this.currentUsers[i].data_id == id)
+            {
                 return this.currentUsers[i].socketIS;
             }
         }
         return false;
     }
 
-    getStatus(id) {
-        for (var i = 0; i < this.currentUsers.length; i++) {
-            if (this.currentUsers[i].data_id.equals(id)) {
-                return { status: this.currentUsers[i].status, last_seen: this.currentUsers[i].last_seen };
-            } else {
-                // logger.info(`${this.currentUsers[i].data_id} is not same as ${id}`);
+    getStatus(id)
+    {
+        for (let i = 0; i < this.currentUsers.length; i++)
+        {
+            if (this.currentUsers[i].data_id.equals(id))
+            {
+                return {status: this.currentUsers[i].status, last_seen: this.currentUsers[i].last_seen};
             }
         }
         return false;
     }
 
-    userGone(id) {
-        for (var i = 0; i < this.currentUsers.length; i++) {
-            if (this.currentUsers[i].socket == id) {
+    userGone(id)
+    {
+        for (let i = 0; i < this.currentUsers.length; i++)
+        {
+            if (this.currentUsers[i].socket == id)
+            {
                 this.currentUsers[i].status = 'offline';
                 this.currentUsers[i].last_seen = new Date().getTime();
-                // logger.info(this.currentUsers);
             }
         }
     }
 
-    getId(socket) {
-        for (var i = 0; i < this.currentUsers.length; i++) {
-            if (this.currentUsers[i].socket == socket) {
-                // logger.info(this.currentUsers);
+    getId(socket)
+    {
+        for (let i = 0; i < this.currentUsers.length; i++)
+        {
+            if (this.currentUsers[i].socket == socket)
+            {
                 return this.currentUsers[i].data_id.toString();
             }
         }
         return false;
     }
-   
-    async  sleep(ms) {
-        return new Promise(resolve => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
+
+    async sleep(ms)
+    {
+        return new Promise(resolve =>
+        {
+            setTimeout(() =>
+            {
+                resolve();
+            }, ms);
         });
     }
 }
 
-module.exports = { Sockets };
+module.exports = {Sockets};
