f7681899d3869868d489547f7f9b511765ca5a09
Khusir-Redapple <77429680+Khusir-Redapple@users.noreply.github.com>
2023-05-03 15:05:27 +0530

    for debugging

--- socket/utils/_tables.js
+++ socket/utils/_tables.js
@@ -1341,7 +1341,7 @@ class _Tables
                     // }                     
                 });
                 // if(count > 1) amount = amount/count; //tie case
-                console.log({amount});
+                console.log('amount', amount);
                 for (let k = 0; k < table.users.length; k++)
                 {
                     for (let j = 0; j < point.length; j++)
