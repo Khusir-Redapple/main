bf541d884df1f6329854fa5e93e62f3021815d2d
Khusir-Redapple <77429680+Khusir-Redapple@users.noreply.github.com>
2023-05-03 20:21:34 +0530

    just for logger

--- socket/controller/_table.js
+++ socket/controller/_table.js
@@ -2378,15 +2378,23 @@ module.exports = {
         // sort the array at descending order.
         playerTurn = playerTurn.sort((a,b) => b-a);
 
-        if(myRoom.users[0].turn != playerTurn[0] && myRoom.users[0].is_active == true) {
-            playersFinalTurn.push(0);
-        } else if(myRoom.users[1].turn != playerTurn[0] && myRoom.users[1].is_active == true) {
-            playersFinalTurn.push(1);
-        } else if(myRoom.users[2].turn != playerTurn[0] && myRoom.users[2].is_active == true) {
-            playersFinalTurn.push(2);
-        } else if(myRoom.users[3].turn != playerTurn[0] && myRoom.users[3].is_active == true) {
-            playersFinalTurn.push(3);
+
+        for (var i = 0; i < myRoom.users.length; i++) {
+            if(myRoom.users[i].turn < playerTurn[0] && myRoom.users[i].is_active == true){
+                playersFinalTurn.push(i);
+            }
         }
+
+        console.log('playerTurn[0]', playerTurn[0]);
+        // if(myRoom.users[0].turn != playerTurn[0] && myRoom.users[0].is_active == true) {
+        //     playersFinalTurn.push(0);
+        // } else if(myRoom.users[1].turn != playerTurn[0] && myRoom.users[1].is_active == true) {
+        //     playersFinalTurn.push(1);
+        // } else if(myRoom.users[2].turn != playerTurn[0] && myRoom.users[2].is_active == true) {
+        //     playersFinalTurn.push(2);
+        // } else if(myRoom.users[3].turn != playerTurn[0] && myRoom.users[3].is_active == true) {
+        //     playersFinalTurn.push(3);
+        // }
         return {'totalTurn':playerTurn[0],'finalTurn':playersFinalTurn}       
     }
 };
