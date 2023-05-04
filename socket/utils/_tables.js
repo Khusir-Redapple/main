d47dabca2e4816d4318d927036ed022b4bf3991d
Khusir-Redapple <77429680+Khusir-Redapple@users.noreply.github.com>
2023-05-04 16:55:01 +0530

    Update _tables.js

--- socket/utils/_tables.js
+++ socket/utils/_tables.js
@@ -1451,7 +1451,7 @@ class _Tables
                     otherRank = table.users[k].rank;
                     let rankCount = 0;
                     UserRankArray.map((ele) => {
-                        if(ele == table.users[k].rank){
+                        if(ele == table.users[k].rank && table.users[k].is_active){
                             rankCount = rankCount+1;
                         }
                     })
