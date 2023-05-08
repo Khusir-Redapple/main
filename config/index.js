8737cf3056fe7e251b7797d9bbb4323452ae8998
Khusir-Redapple <77429680+Khusir-Redapple@users.noreply.github.com>
2023-05-08 15:07:23 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -17,8 +17,8 @@ class config
         this.diceGenerateRange = 25; // No. of dice value generate at the time of game start for each player
         this.safeZone = [1, 14, 27, 40, 22, 35, 9, 48]; // Pawn safe Zone.
         this.starPosition = [21]; // Pawn start position.
-        this.gameTime = 10; // Game EndTime is 10 minutes.
-        this.turnTimer = 1; // Dice roll time 10 sec.
+        this.gameTime = 1; // Game EndTime is 10 minutes.
+        this.turnTimer = 10; // Dice roll time 10 sec.
         this.countDownTime = 30; // previously it was 10 sec.
         this.pawnMoveTimer = 0.08; // Pawn move timer.
         this.noOfPlayersInTournament = [2, 3, 4]; //Game start if number of player matches in array value.
