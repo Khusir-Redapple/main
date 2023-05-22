a9dbe2dcfcdec40281e1a03ba5fc36925001fdf1
Khusir-Redapple <77429680+Khusir-Redapple@users.noreply.github.com>
2023-05-22 12:41:58 +0530

    Update _tables.js

--- socket/utils/_tables.js
+++ socket/utils/_tables.js
@@ -920,12 +920,12 @@ class _Tables
     }
     async sendToSqsAndResetGamePlayData(room, myRoom, gamePlayData, myPos)
     {
-        // let i = this.gamePlayData.findIndex((x) => x.room == room);
-        // let gamePlayData = await redisCache.getRecordsByKeyRedis('gamePlay_'+room);
-        //console.log('SQS SEND DATA  ============> ', gamePlayData, myPos);
         let me = myRoom.users[myPos];
-        gamePlayData.data.User = me.numeric_id,
-        await sendMessage(gamePlayData);
+        gamePlayData.data.User = me.numeric_id;
+        if('data' in gamePlayData) {
+            await sendMessage(gamePlayData);
+        }
+        
         //send through SQS
         await this.resetGamePlayData(room, myRoom, gamePlayData,myPos);
     }
