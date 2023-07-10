43cf459b4946b2ef5275341e05605b1b67b1c42a
Khusir-Redapple <77429680+Khusir-Redapple@users.noreply.github.com>
2023-07-10 18:51:14 +0530

    for debugging

--- socket/index.js
+++ socket/index.js
@@ -491,8 +491,6 @@ module.exports = function (io, bullQueue) {
                 await redisCache.addToRedis(myRoom.room, myRoom);
                 await redisCache.addToRedis('gamePlay_' + myRoom.room, gamePlayData);
                 // console.log('GAME-PLAY-DATA-4', JSON.stringify(gamePlayData));
-                callback(response.callback);
-                if (response.callback.status == 1) processEvents(response, myRoom, socket);
                 await bullQueue.add(
                     {
                         name: "playerTurnQueue",
@@ -503,8 +501,8 @@ module.exports = function (io, bullQueue) {
                     }
                 );
 
-                // callback(response.callback);
-                // if (response.callback.status == 1) processEvents(response, myRoom, socket);
+                callback(response.callback);
+                if (response.callback.status == 1) processEvents(response, myRoom, socket);
             }
             catch (err) {
                 console.error("tournament_move_made", err);
