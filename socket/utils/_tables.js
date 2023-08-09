8336ecfea214590f54530ae376692bb923bedb44
Khusir-Redapple <77429680+Khusir-Redapple@users.noreply.github.com>
2023-08-09 13:24:51 +0530

    new random number generate logic implemented

--- socket/utils/_tables.js
+++ socket/utils/_tables.js
@@ -94,6 +94,7 @@ class _Tables
                     diceValue : pl == 0 ? JSON.parse((JSON.stringify(randomRumber))) : JSON.parse((JSON.stringify(shuffleNumberForOtherPlayer)))
                 };
             }
+            console.log(JSON.stringify(table_i));
             await redisCache.addToRedis(table.room, table_i);
             //this.tables.push(table_i);
             resolve(table_i.room);
